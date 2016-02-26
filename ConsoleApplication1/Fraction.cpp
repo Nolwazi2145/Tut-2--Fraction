8ba05059347c1bb02d73c306d931c71696420610
Nolwazi2145 <214553811@stu.ukzn.ac.za>
2016-02-26 12:17:08 +0200

    started

---
+++ ConsoleApplication1/Fraction.cpp
@@ -0,0 +1,20 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+class Fraction
+{
+private:
+
+	int numerator;
+	int denomunator;
+
+public:
+	~Fraction();
+	Fraction(int num = 0, int denom = 1)
+	{
+	
+	
+	
+	
+	
+   };
+#endif
---
+++ ConsoleApplication1/Fraction.h
@@ -0,0 +1,14 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+class Fraction
+{
+private:
+
+	int numerator;
+	int denomunator;
+
+public:
+	Fraction( int num = 0 , int denom = 1);
+	~Fraction();
+};
+#endif
