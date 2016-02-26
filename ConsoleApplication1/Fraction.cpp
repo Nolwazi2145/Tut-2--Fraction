6ea48b67814030e5b3dc1c5b82123ab55c0862a2
Nolwazi2145 <214553811@stu.ukzn.ac.za>
2016-02-26 14:14:46 +0200

    declaration of member functions.

--- ConsoleApplication1/Fraction.cpp
+++ ConsoleApplication1/Fraction.cpp
@@ -5,16 +5,42 @@ class Fraction
 private:
 
 	int numerator;
-	int denomunator;
+	int denominator;
 
 public:
 	~Fraction();
 	Fraction(int num = 0, int denom = 1)
+	{  
+		num = numerator;
+		denom = denominator; 
+    }
+	void setFract(int, int)
 	{
-	
-	
-	
-	
-	
+	  
+	}
+	void getFract(int, int)
+	{
+
+	}
+	Fraction fadd(Fraction add)
+	{
+
+	}
+	Fraction fsubtract(Fraction subtract)
+	{
+      
+	}
+	Fraction fmultiply(Fraction multiply)
+	{
+		numerator = numerator * multiply.numerator;
+		denominator = denominator * multiply.denominator;
+		return multiply;
+	}
+	Fraction fdivide(Fraction divide)
+	{
+		numerator = numerator * divide.denominator;
+		denominator = denominator * divide.numerator;
+		return divide;
+	}
    };
 #endif
